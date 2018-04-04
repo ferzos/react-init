894ce8d9a5c6dd726485a51244ec577dac5920e9
Ferdinand <ferdifenrir@gmail.com>
2018-04-05 00:37:58 +0700

    setting up for redux

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,6 +3,9 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import * as Icons from '@fortawesome/fontawesome-free-solid';
 import logo from '../assets/logo.svg';
 import Heading from '../components/Heading';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { fetchText } from '../actions/app';
 
 class App extends Component {
   render() {
@@ -14,19 +17,40 @@ class App extends Component {
     const style2 = {
       textAlign: 'right',
       marginRight: '14%',
+      cursor: 'pointer',
     };
 
     return (
       <div style={style}>
         <div className="color-primary">
-          <Heading text="Welcome to Ferzos React Template" logo={logo} />
+          <Heading text={this.props.app.headingText} logo={logo} />
         </div>
-        <div style={style2} className="color-secondary text-sm">
-          Ferzos <FontAwesomeIcon icon={Icons.faCopyright} />
+        <div
+          style={style2}
+          className="color-secondary text-sm"
+          onClick={() => this.props.fetchText('Ferzos')}
+        >
+          {this.props.app.copyrightText}{' '}
+          <FontAwesomeIcon icon={Icons.faCopyright} />
         </div>
       </div>
     );
   }
 }
 
-export default App;
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      fetchText: fetchText,
+    },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    app: state.app,
+  };
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(App);
